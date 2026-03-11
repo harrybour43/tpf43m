d4d91ee7c3bbe702ab05e53e958e95f744e0cb60
harrybour43 <harrybour.foto@gmail.com>
2026-03-11 19:25:55 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Alteração gerada em: 10/03/2026 - 11:00
-const CACHE_NAME = 'clara-cache-v6.3.0'; // Mude aqui para atualizar o código visual
+const CACHE_NAME = 'clara-cache-v6.3.1'; // Mude aqui para atualizar o código visual
 const ASSETS = [
   '/clara/',
   '/clara/index.html',
